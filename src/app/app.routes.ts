4094123054bc4c02bc6a3d7d830da0ee58862556
Pol Pérez Castillo <perezcastillopol@gmail.com>
2025-09-30 12:46:57 +0200

    rutas definidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {Home} from '../components/home/home';
+import {UserDetail} from '../components/user-detail/user-detail';
+import {UserForm} from '../components/user-form/user-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: Home},
+  {path: 'user/:id', component: UserDetail},
+  {path: 'newuser', component: UserForm},
+  {path: 'updateuser/:id', component: UserForm}
+];
