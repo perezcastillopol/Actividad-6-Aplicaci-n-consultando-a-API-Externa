752e8b1cbbf4ea63b3cd99a38e25a50d645db986
Pol Pérez Castillo <perezcastillopol@gmail.com>
2025-09-30 13:18:39 +0200

    metodos CRUD del servicio de user creados

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,35 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
   httpClient = inject(HttpClient);
+
+  private apiUrl = 'https://peticiones.online/users';
+
+  getAllUsers(): Observable<User[]> {
+    return this.httpClient.get<any>(this.apiUrl).pipe(
+      map(resp => resp.data)
+    );
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.httpClient.get<User>(`${this.apiUrl}/${id}`);
+  }
+
+  createNewUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user);
+  }
+
+  updateUser(id: number, user: User): Observable<User> {
+    return this.httpClient.put<User>(`${this.apiUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/${id}`);
+  }
+
 }
