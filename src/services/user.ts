8e07719752a5764440942a2f28d63f2c84d536cb
Pol Pérez Castillo <perezcastillopol@gmail.com>
2025-09-30 13:08:06 +0200

    inject httpclient y interfaz de user creada

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
-  
+  httpClient = inject(HttpClient);
 }
